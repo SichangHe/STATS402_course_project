7729797a7426ffbb2d2d04c86d8f226ffa757471
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2024-04-11 13:55:29 +0800

    dummy rust python project

---
+++ battlesnake_train/battlesnake_gym/python/battlesnake_gym/__init__.py
@@ -0,0 +1,3 @@
+from battlesnake_gym._lowlevel import hello
+
+__all__ = ["hello"]
---
+++ battlesnake_train/src/battlesnake_train/gym.py
@@ -0,0 +1,18 @@
+from gymnasium import Env
+
+
+class BattlesnakeEnv(Env):
+    def __init__(self):
+        super().__init__()
+
+    def reset(self):
+        pass
+
+    def step(self, action):
+        pass
+
+    def render(self):
+        pass
+
+    def close(self):
+        pass

8cdfa59c52829e7c09166a547138215d13b5ca1c
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2024-04-20 14:28:50 +0800

    switch to pettingzoo&python3.11

--- battlesnake_train/battlesnake_gym/python/battlesnake_gym/__init__.py
+++ battlesnake_train/battlesnake_gym/python/battlesnake_gym/__init__.py
@@ -1,39 +1,57 @@
-from typing import Any
+from typing import Any, Final
 
 import numpy as np
-from gymnasium import Env
+from gymnasium import spaces
+from pettingzoo import ParallelEnv
 
 from battlesnake_gym._lowlevel import SnakeGame, hello
 
+BOARD_SIZE: Final = 11
+N_SNAKES: Final = 4
 
-class BattlesnakeEnv(Env):
+PADDED_SIZE: Final = BOARD_SIZE * 2 - 1
+N_LAYERS: Final = 10
+
+OBSERVATION_SPACE: Final = spaces.Box(-1.0, 1.0, (N_LAYERS, PADDED_SIZE, PADDED_SIZE))
+ACTION_SPACE: Final = spaces.Discrete(4)
+
+
+class BattlesnakeEnv(ParallelEnv):
+    metadata = {
+        # TODO: render_modes
+        "render_modes": [],
+    }
+
     def __init__(self):
         self.snake_game = SnakeGame()
 
     # override
     def reset(
         self,
-        *,
         seed: int | None = None,
         options: dict[str, Any] | None = None,
     ):
         super().reset(seed=seed)
         # TODO: Use seed and options.
+        _ = options
         self.snake_game.reset()
         # TODO: Implement.
-        observation = np.zeros((1, 1))
-        info: dict[str, Any] = {}
-        return observation, info
+        observations = self._observations()
+        infos = self._make_infos()
+        return observations, infos
 
     # override
-    def step(self, action):
+    def step(self, actions: dict[int, int]):
         # TODO: Implement.
-        observation = np.zeros((1, 1))
-        reward = 0.0
-        terminated = False
-        truncated = False
-        info: dict[str, Any] = {}
-        return observation, reward, terminated, truncated, info
+        actions_converted = [actions[i] for i in range(N_SNAKES)]
+        # TODO: Use result.
+        _ = self.snake_game.step(actions_converted)
+        infos = self._make_infos()
+        rewards = {i: 0.0 for i in range(N_SNAKES)}
+        terminations = {i: False for i in range(N_SNAKES)}
+        truncations = {i: False for i in range(N_SNAKES)}
+        observations = self._observations()
+        return observations, rewards, terminations, truncations, infos
 
     # override
     def render(self):
@@ -41,8 +59,21 @@ class BattlesnakeEnv(Env):
         pass
 
     # override
-    def close(self):
-        pass
+    def observation_space(self, agent: int):
+        _ = agent
+        return OBSERVATION_SPACE
+
+    # override
+    def action_space(self, agent: int):
+        _ = agent
+        return ACTION_SPACE
+
+    def _observations(self):
+        # TODO: Get from self.game.
+        return {i: np.zeros((1, 1), dtype=np.float32) for i in range(N_SNAKES)}
+
+    def _make_infos(self) -> dict[int, dict[str, Any]]:
+        return {i: {} for i in range(N_SNAKES)}
 
 
 __all__ = ["hello"]

a408a9fe6aad3206d2d73438ae1ac8b10d9667e4
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2024-04-21 21:38:33 +0800

    fix animation;init next plan

--- battlesnake_train/src/battlesnake_train/__init__.py
+++ battlesnake_train/src/battlesnake_train/__init__.py
@@ -1,5 +1,5 @@
-from timeit import timeit
 from time import sleep
+from timeit import timeit
 
 import numpy as np
 from stable_baselines3 import PPO
@@ -9,6 +9,7 @@ from stable_baselines3.ppo import MlpPolicy
 from battlesnake_gym import make_battlesnake_env
 
 CLEAR = "\033[2J\033[H"
+
 
 def mini_train():
     env = make_battlesnake_env()
@@ -20,13 +21,15 @@ def mini_train():
     mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=1)
     print(f"reward={mean_reward:.2f} +/- {std_reward:.2f}")
 
-    done = [True]
+    cummulative_done = [True, True, True, True]
     while True:
         # fmt: off
-        if np.any(done):
+        if np.all(cummulative_done):
+            cummulative_done = [False, False, False, False]; 
             obs = env.reset(); print(f"{CLEAR}{env.render()}")
             sleep(1)
         action, _ = model.predict(obs); obs, _, done, _ = env.step(action); print(f"{CLEAR}{env.render()}") # type: ignore
+        cummulative_done = [cummulative_done[i] or bool(done[i]) for i in range(4) ]
         sleep(0.2)
 
 

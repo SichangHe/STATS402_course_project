3858c869dd594b27d0db43d725b121a965acca26
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2024-04-22 15:32:09 +0800

    resume training

--- battlesnake_train/src/battlesnake_train/__init__.py
+++ battlesnake_train/src/battlesnake_train/__init__.py
@@ -1,3 +1,5 @@
+import os
+import re
 from time import sleep
 from timeit import timeit
 
@@ -28,19 +30,42 @@ def mini_train():
             cummulative_done = [False, False, False, False]; 
             obs = env.reset(); print(f"{CLEAR}{env.render()}")
             sleep(1)
-        action, _ = model.predict(obs); obs, _, done, _ = env.step(action); print(f"{CLEAR}{env.render()}") # type: ignore
+        action, _ = model.predict(obs); obs, rewards, done, _ = env.step(action); print(f"{CLEAR}{env.render()}") # type: ignore
         cummulative_done = [cummulative_done[i] or bool(done[i]) for i in range(4) ]
         sleep(0.2)
 
 
 def large_train():
     env = sb3_vec_env()
-    model = PPO(MlpPolicy, env)
+    last_trial_and_model = find_last_model()
+
+    if last_trial_and_model:
+        last_trial = last_trial_and_model[0]
+        model = PPO.load(last_trial_and_model[1], env=env)
+        print(f"Resuming training from `{last_trial_and_model[1]}`")
+    else:
+        last_trial = -1
+        model = PPO(MlpPolicy, env)
+        print("Starting training from scratch.")
+
     learn = lambda: model.learn(0x100_000, log_interval=0x1_000, progress_bar=True)
-    for trial in range(1000_000):
+
+    for trial in range(last_trial + 1, 1000_000):
         execution_time = timeit(learn, number=1)
         print(f"Trial {trial}: took {execution_time:.2f} seconds.")
         model.save(f"ppo-mlp-battlesnake{trial}.model")
+
+
+def find_last_model():
+    last_trial_and_model = None
+    model_regex = re.compile(r"ppo-mlp-battlesnake(\d+)\.model")
+    for file in os.listdir():
+        match = model_regex.match(file)
+        if match:
+            trial = int(match.group(1))
+            if last_trial_and_model is None or trial > last_trial_and_model[0]:
+                last_trial_and_model = trial, file
+    return last_trial_and_model
 
 
 def hello() -> str:

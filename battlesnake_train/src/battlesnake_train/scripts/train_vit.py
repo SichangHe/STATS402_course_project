a4e8179a87c4370b374b9f23aad3849b7cdab463
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2024-04-28 12:37:14 +0000

    dial down entropy coefficient

--- battlesnake_train/src/battlesnake_train/scripts/train_vit.py
+++ battlesnake_train/src/battlesnake_train/scripts/train_vit.py
@@ -13,7 +13,7 @@ from battlesnake_train.feature import (
 from battlesnake_train.ppo import DynPPO
 
 env = BattlesnakeEnv()
-model = DynPPO.load_trial(env, save_model_name="vit-ent", ent_coef=0.1)
+model = DynPPO.load_trial(env, save_model_name="vit-2ent", ent_coef=0.01)
 if model is None:
     policy_kwargs = dict(
         features_extractor_class=ViTFeatureExtractor,
@@ -23,8 +23,8 @@ if model is None:
     model = DynPPO(
         MlpPolicy,
         env,
-        save_model_name="vit-ent",
-        ent_coef=0.1,
+        save_model_name="vit-2ent",
+        ent_coef=0.01,
         policy_kwargs=policy_kwargs,
         verbose=1,
     )
